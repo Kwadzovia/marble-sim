c795fc660092483feae0ea2fafc6d5eaf916b21f
Megan <meganholmes100@gmail.com>
2020-02-27 20:00:38 -0800

    Init Data & Output to CMD

---
+++ printToCMD.m
@@ -0,0 +1,4 @@
+function printToCMD(t,xpos,ypos,xvelocity,yvelocity,xacceleration,yacceleration)
+
+fprintf("t= " + t/10 + ", x= " + xpos + ", y= " + ypos + ", xVel= " + xvelocity + ", yVel= " + yvelocity + ", xAccel= " + xacceleration + ", Yaccel= " + yacceleration)
+fprintf(newline)
