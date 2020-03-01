9ccffa46062f84a5cef14bb77f70691808384373
Megan <meganholmes100@gmail.com>
2020-02-29 16:18:09 -0800

    Add basic update feature WRT time
    Added incrementation of positions, velocity, and accelerations with respect to time

--- printToCMD.m
+++
@@ -1,4 +0,0 @@
-function printToCMD(t,xpos,ypos,xvelocity,yvelocity,xacceleration,yacceleration)
-
-fprintf("t= " + t/10 + ", x= " + xpos + ", y= " + ypos + ", xVel= " + xvelocity + ", yVel= " + yvelocity + ", xAccel= " + xacceleration + ", Yaccel= " + yacceleration)
-fprintf(newline)
