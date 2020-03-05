fe461dafee79b5460982c7af660e0bee7900efee
Megan <meganholmes100@gmail.com>
2020-03-04 16:05:40 -0800

    Implement visual of map
    Created a visual of the map using a scatter plot. Doesn't look too pretty but it works. still need to figure out how to stop clashing with draw function.

--- main.m
+++ main.m
@@ -28,37 +28,59 @@ map = zeros(600,600);
 %%value of each part of the line to 1 in map.
 for a = 1:length(ramp_list)
     m = (ramp_list(a).endY - ramp_list(a).startY)/(ramp_list(a).endX - ramp_list(a).startX); %%slope
-    b = ramp_list(a).startY - m*ramp_list(a).startX;
-    for i = ramp_list(a).startX : ramp_list(a).endX %%only check within bounds of the line
-        for j = ramp_list(a).startY : ramp_list(a).endY %%only check within bounds of the line
-            if (m*i + b) == j %%if the value of i satisfies the line equation then this point is on the line
+    b = ramp_list(a).startY - m*(ramp_list(a).startX);
+    
+    %%for debugging
+    fprintf("Ramp " + a + " is y = " + m + "x + " + b);
+    fprintf(newline);
+    
+    for i = min(ramp_list(a).startX, ramp_list(a).endX) : max(ramp_list(a).startX, ramp_list(a).endX) %%only check within bounds of the line
+        for j = min(ramp_list(a).startY, ramp_list(a).endY) : max(ramp_list(a).startY, ramp_list(a).endY) %%only check within bounds of the line
+            if m*i + b == j %%if the value of i satisfies the line equation then this point is on the line
                 map(i,j) = 1;
             end
         end
     end
 end
 
-%%init the graph visualization
-figure
-set(gcf, 'Position',  [100, 100, 600, 600]) %sets graph window size and position
+solidX = [];
+solidY = [];
+solid_index = 1;
+for i = 1:600
+    for j = 1:600
+        if map(i,j) == 1
+            solidX(solid_index) = i;
+            solidY(solid_index) = j;
+            solid_index = solid_index + 1;
+        end
+    end
+end
 
+figure
+set(gcf, 'Position',  [1, 1, 600, 600]) %sets graph window size and position
+scatter(solidX,solidY);
+pbaspect([1,1,1]); %sets aspect ratio to 1:1
+axis([0 600 0 600]); %sets plot size to 600 by 600
+
+%%init the graph visualization
+%%figure
+%%set(gcf, 'Position',  [1, 1, 600, 600]) %sets graph window size and position
 %% each second is 10 values for t. example: 20 seconds is t=200
-for t = 0:100
+for t = 0:400
     
     %%line below is update_tick which updates everything based on previous
     %%conditions.
     [position, linear_velocity, linear_acceleration, angular_velocity, angular_acceleration] = update_tick(position, linear_velocity, linear_acceleration, angular_velocity, angular_acceleration);
     
     %%print output to command window
-    fprintf("t= " + t/10 + ", x= " + position(1) + ", y= " + position(2))
-    fprintf(", xVelLin= " + linear_velocity(1) + ", yVelLin= " + linear_velocity(2))
-    fprintf(", xAccelLin= " + linear_acceleration(1) + ", yAccelLin= " + linear_acceleration(2))
-    fprintf(", xVelANG= " + angular_velocity(1)+ ", yVelANG= " + angular_velocity(2))
-    fprintf(", xAccelANG= " + angular_acceleration(1) + ", yAccelANG = " + angular_acceleration(2))
-    fprintf(", xForce = " + force(1) + ", yForce= " + force(2))
-    fprintf(newline)
+    %%fprintf("t= " + t/10 + ", x= " + position(1) + ", y= " + position(2))
+    %%fprintf(", xVelLin= " + linear_velocity(1) + ", yVelLin= " + linear_velocity(2))
+    %%fprintf(", xAccelLin= " + linear_acceleration(1) + ", yAccelLin= " + linear_acceleration(2))
+    %%fprintf(", xVelANG= " + angular_velocity(1)+ ", yVelANG= " + angular_velocity(2))
+    %%fprintf(", xAccelANG= " + angular_acceleration(1) + ", yAccelANG = " + angular_acceleration(2))
+    %%fprintf(", xForce = " + force(1) + ", yForce= " + force(2))
+    %%fprintf(newline)
     
     %%draws the sphere
-    draw(position)
-    pause(0.01) %%done so we can actually see whats happening
+    %%draw(position)
 end
--- ramp_list.m
+++ ramp_list.m
@@ -3,17 +3,17 @@
 function [list] = ramp_list()
 
     %%ramp 1
-    ramp(1).startX = 1;
+    ramp(1).startX = 100;
     ramp(1).startY = 500;
-    ramp(1).endX = 100;
-    ramp(1).endY = 500;
+    ramp(1).endX = 300;
+    ramp(1).endY = 400;
     ramp(1).friction = 0;
     
     %%ramp2
-    ramp(2).startX = 300;
-    ramp(2).startY = 200;
-    ramp(2).endX = 100;
-    ramp(2).endY = 100;
+    ramp(2).startX = 400;
+    ramp(2).startY = 300;
+    ramp(2).endX = 150;
+    ramp(2).endY = 200;
     ramp(2).friction = 0;
     
     for i = 1:length(ramp)
