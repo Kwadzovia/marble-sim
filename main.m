bd140d9a28bd25bd3c7b309f0f421569309e8fee
Megan <meganholmes100@gmail.com>
2020-03-07 19:56:50 -0700

    Create collision_handle
    Function that will handle all collisions that take place (probably by calling other scripts).
    
    Moved basic physics into there from main.

--- main.m
+++ main.m
@@ -51,24 +51,9 @@ for t = 0:4000
         return %%kills the program if the marble leaves
     end
     
-    if(col_occur)
-        %%do some mathy stuff, these values are just placeholder for
-        %%testing
-        if colY < position(2) %%if collision was below the centre
-            linear_acceleration(2) = 0;
-            linear_velocity(2) = 0;
-        end
-        %%if collision was to the right of the sphere and the acceleration was in that direction
-        if colX > position(1) && linear_acceleration(1)>0
-            linear_acceleration(1) = -linear_acceleration(1); %invert the acceleration
-            linear_velocity(1) = 0; %reset velocity
-        end
-        %%if collision was to the left of the sphere and the acceleration was in that direction
-        if colX < position(1) && linear_acceleration(1)<0
-            linear_acceleration(1) = -linear_acceleration(1); %%invert the acceleration
-            linear_velocity(1) = 0; %%reset velocity
-        end
-    else
+    if(col_occur) %%handles the collision if there was one
+        [position, linear_velocity, linear_acceleration, angular_velocity, angular_acceleration] = collision_handle(colX, colY, position, linear_velocity, linear_acceleration, angular_velocity, angular_acceleration);
+    else %%otherwise keep gravitational acceleration going
         linear_acceleration(2) = gravity/mass;
     end
     
