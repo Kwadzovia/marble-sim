8728f7e4da36ae2f968c8c491301eaf88254ef00
Megan <meganholmes100@gmail.com>
2020-04-06 17:07:24 -0700

    Acceleration calculations
    calculates linear and angular accelerations. 50% chance to work tbh

--- circle_physics.m
+++ circle_physics.m
@@ -1,22 +1,28 @@
-%%circular physics
-function [v] = circle_physics(position, velocity)
+%%circular physics. returns lin accel and angular accel
+function [accel, angular] = circle_physics(position)
+%%inits
 r = 990;
+marbr = 5/1000;
 g = 9.81;
+friction = 0.22;
 centre = [260 1500];
+
+%%section might be useless
 endpositionx = 40;
 endpositiony = 510;
 deltaxend = centre(1) - endpositionx;
 deltayend = centre(2) - endpositiony;
 thetaend = tan(deltayend/deltaxend);
 
+%%determinging theta
 deltaxstart = centre(1)-position(1);
 deltaystart = centre(2)-position(2);
 thetastart = tan(deltaystart/deltaxstart);
 
-
-v(1) = 10*r*g*sin(thetastart)/7 + velocity;
-
-
-
+%%accel calculations
+accelx = friction*g*cos(thetastart)*cos(thetastart)/2*5;
+accely = friction*g*cos(thetastart)*sin(thetastart)/2*5;
+angular = accelx/marbr;
+accel = [accelx, accely];
 
 end
